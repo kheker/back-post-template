2f71d88e9b4dd145a7d4dcaa5474acc306552ed0
kevin <cronox030@gmail.com>
2017-12-20 20:34:01 -0430

    modelo de comentarios modificado.

--- src/modules/comments/comment.model.js
+++ src/modules/comments/comment.model.js
@@ -1,29 +1,30 @@
 import mongoose, { Schema } from 'mongoose';
 
 const CommentSchema = new Schema({
-  userId:{
+  postRefId:{
+    type:Schema.Types.ObjectId,
+    ref:'Post',
+    required:true
+  },
+  userRef:{
     type:Schema.Types.ObjectId,
     ref: 'User'
   },
-  messages:[
-    {
-      eval:{
-        type:Number,
-        required:[true, 'Califica al freelancer'],
-        min:true,
-        default:0,
-      },
-      message: {
-        type: String,
-        required:[true, 'Has un comentario'],
-        minlength:[10, 'Tu comentario debe ser mas largo'],
-      },
-      userRef:{
-        type:Schema.Types.ObjectId,
-        ref: 'User'
-      }
-    }
-  ],
+  hireUserId:{
+    type:Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  eval:{
+    type:Number,
+    required:[true, 'Califica al freelancer'],
+    min:true,
+    default:0,
+  },
+  message: {
+    type: String,
+    required:[true, 'Has un comentario'],
+    minlength:[10, 'Tu comentario debe ser mas largo'],
+  },
 },{timestamps:true});
 
 CommentSchema.index({userId: 2}, {unique: true});
